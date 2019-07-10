6330d9bcd16d2884e58d64d5107251b745fc2b84
Hary <agus.hary16@gmail.com>
2019-07-10 15:31:03 +0800

    Update receiving data

--- app/Controllers/Http/NodeDatumController.js
+++ app/Controllers/Http/NodeDatumController.js
@@ -5,7 +5,7 @@ const Database = use("Database");
 class NodeDatumController {
 
   async eventData({ params,request, response }) {
-   let data =  await Database.raw("call nodeData('"+params.id+"','"+ params.pressure+"','"+ params.flow+"','0')");
+   let data =  await Database.raw("call nodeData('"+params.id+"','"+parseFloat(params.pressure)+"','"+ parseFloat(params.flow)+"','0')");
     return response.json({succes:1}, 200);
   }
 }
